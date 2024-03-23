be48e9a51fa63b0080555e8355c61047d8140f4b
yared <34055381+yaredaddisu@users.noreply.github.com>
2024-03-23 07:01:56 -0700

    Update MyPortfolio.jsx

--- src/Pages/Home/MyPortfolio.jsx
+++ src/Pages/Home/MyPortfolio.jsx
@@ -9,7 +9,7 @@ export default function MyPortfolio() {
           <h2 className="section--heading">My Portfolio</h2>
         </div>
         <div>
-          <button className="btn btn-github">
+          <router-link to="https://github.com/yaredaddisu"  className="btn btn-github">
             <svg
               xmlns="http://www.w3.org/2000/svg"
               width="32"
@@ -25,7 +25,7 @@ export default function MyPortfolio() {
               />
             </svg>
             Visit My GitHub
-          </button>
+          </router-link >
         </div>
       </div>
       <div className="portfolio--section--container">
