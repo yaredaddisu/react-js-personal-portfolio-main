59be05676395b814020b1e0459d663a15a04ec40
yared <34055381+yaredaddisu@users.noreply.github.com>
2024-03-23 06:59:24 -0700

    Update Footer.jsx

--- src/Pages/Home/Footer.jsx
+++ src/Pages/Home/Footer.jsx
@@ -172,7 +172,7 @@ function Footer() {
       </div>
       <hr className="divider" />
       <div className="footer--content--container">
-        <p className="footer--content">Made with 💖 by John</p>
+        <p className="footer--content">Made with 💖 by Yared Addisu</p>
         <div className="footer--social--icon">
           <ul>
             <li>
