5c43e2e3f58e5075b5432ca716c978d08be04c18
yared <34055381+yaredaddisu@users.noreply.github.com>
2024-03-23 06:02:22 -0700

    update

--- src/Pages/Home/Footer.jsx
+++ src/Pages/Home/Footer.jsx
@@ -6,7 +6,7 @@ function Footer() {
     <footer className="footer--container">
       <div className="footer--link--container">
         <div>
-          <img src="./img/logo.svg" alt="Logoipsum" />
+          <img src="./img/logo.png" alt="Lomi" />
         </div>
         <div className="footer--items">
           <ul>
