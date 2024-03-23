3a38a4c949e1ad287ce95791cd5f339b42e5a67b
yared <34055381+yaredaddisu@users.noreply.github.com>
2024-03-23 05:52:39 -0700

    update

--- src/Pages/Home/Navbar.jsx
+++ src/Pages/Home/Navbar.jsx
@@ -35,7 +35,7 @@ function Navbar() {
   return (
     <nav className={`navbar ${navActive ? "active" : ""}`}>
       <div>
-        <img src="./img/logo.png" alt="Lomi" />
+        <img src="./img/logo.svg" alt="Lomi" />
       </div>
       <a
         className={`nav__hamburger ${navActive ? "active" : ""}`}
